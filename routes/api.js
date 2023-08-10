d5f893f463711b33ba5aef05deecb8343509ea20
ll-nick <github@le-large.net>
2023-08-10 11:22:03 +0200

    implement routes

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,30 @@
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
+
   let solver = new SudokuSolver();
 
   app.route('/api/check')
     .post((req, res) => {
+      try {
+        const puzzleStr = req.body.puzzle;
+        const coordinateStr = req.body.coordinate;
+        const value = req.body.value;
 
+        res.json(solver.checkValuePlacement(puzzleStr, coordinateStr, value));
+      } catch (err) {
+        res.json({ error: err.message });
+      }
     });
-    
+
   app.route('/api/solve')
     .post((req, res) => {
+      try {
+        const solution = solver.solve(req.body.puzzle);
 
+        res.json({ solution: solution });
+      } catch (err) {
+        res.json({ error: err.message });
+      }
     });
 };
